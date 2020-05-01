d0852f8c4e3ef0e421d21a210eeaa42c3f84eb23
tsaniawarda2 <63621268+tsaniawarda2@users.noreply.github.com>
2020-05-01 22:31:26 +0700

    add pertemuan2

---
+++ kuliah/pertemuan12/detail.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login'])) {
+  header("Location: login.php");
+  exit;
+}
+require 'functions.php';
+
+// ambil id dari URL
+$id = $_GET['id'];
+// query mahasiswa berdasarkan id
+$m = query("SELECT * FROM mahasiswa WHERE id = $id");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Detail Mahasiswa</h3>
+  <ul>
+    <li><img src="img/<?= $m['gambar']; ?>" alt=""></li>
+    <li>NRP : <?= $m['nrp']; ?></li>
+    <li>Nama : <?= $m['nama']; ?></li>
+    <li>Email : <?= $m['email']; ?></li>
+    <li>Jurusan : <?= $m['jurusan']; ?></li>
+    <li><a href="ubah.php?id=<?= $m['id']; ?>">ubah</a> | <a href="hapus.php?id=<?= $m['id']; ?>" onclick="return confirm('Apakah Anda yakin?');">hapus</a></li>
+    <li><a href="index.php">Kembali ke daftar mahasiswa</a></li>
+  </ul>
+</body>
+
+</html>
